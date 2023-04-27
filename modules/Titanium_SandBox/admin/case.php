bca126881226840a2263db2c0a4ab0161317dfca
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-27 18:15:58 -0400

    Update case.php

--- modules/Titanium_SandBox/admin/case.php
+++ modules/Titanium_SandBox/admin/case.php
@@ -3,25 +3,17 @@ if (!defined('ADMIN_FILE')) {
    exit('THIS FILE WAS NOT CALLED WITHIN ADMINISTRATION');
 }
 
-$pnt_module = basename(dirname(dirname(__FILE__)));
-get_lang($pnt_module);
+$module = basename(dirname(dirname(__FILE__)));
+get_lang($module);
 
 $op = $_GETVAR->get('op', 'request', 'string');
 
 switch($op) 
 {
-    case 'TitaniumSandboxMenu';
-	include(NUKE_MODULES_DIR.$pnt_module.'/admin/index.php');
-	break;
+	case 'TitaniumSandboxMenu';
+    case 'fixbots';
     case 'step1';
-	include(NUKE_MODULES_DIR.$pnt_module.'/admin/index.php');
-	break;
     case 'step2';
-	include(NUKE_MODULES_DIR.$pnt_module.'/admin/index.php');
-	break;
-    case 'step3';
-	include(NUKE_MODULES_DIR.$pnt_module.'/admin/index.php');
-	break;
 	case 'a':
     case 'b':
     case 'c':
@@ -47,7 +39,7 @@ switch($op)
     case 'x':
     case 'y':
     case 'z':
-        include(NUKE_MODULES_DIR.$pnt_module.'/admin/index.php');
+        include(NUKE_MODULES_DIR.$module.'/admin/index.php');
     break;
 }
 
