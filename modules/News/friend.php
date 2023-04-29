ee84a3c4155b79a5bdde137a5f6656e587230ebd
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-29 17:38:24 -0400

    Update friend.php
    /* Applied rules: Ernest Allen Buffington (TheGhost) 04/29/2023 5:37 PM
     * AddDefaultValueForUndefinedVariableRector (https://github.com/vimeo/psalm/blob/29b70442b11e3e66113935a2ee22e165a70c74a4/docs/fixing_code.md#possiblyundefinedvariable)
     * NullToStrictStringFuncCallArgRector
     */

--- modules/News/friend.php
+++ modules/News/friend.php
@@ -24,11 +24,16 @@
       Nuke Patched                             v3.1.0       06/26/2005
  ************************************************************************/
 
+/* Applied rules: Ernest Allen Buffington (TheGhost) 04/29/2023 5:37 PM
+ * AddDefaultValueForUndefinedVariableRector (https://github.com/vimeo/psalm/blob/29b70442b11e3e66113935a2ee22e165a70c74a4/docs/fixing_code.md#possiblyundefinedvariable)
+ * NullToStrictStringFuncCallArgRector
+ */
+ 
 if (!defined('MODULE_FILE')) {
    die('You can\'t access this file directly...');
 }
 if (!file_exists("includes/nukesentinel.php")) {
-   if (stristr($_SERVER['QUERY_STRING'],'%25')) redirect("index.php");
+   if (stristr((string) $_SERVER['QUERY_STRING'],'%25')) redirect("index.php");
    }
 $module_name = basename(dirname(__FILE__));
 get_lang($module_name);
@@ -40,12 +45,14 @@ if (!is_user()) {
 }
 
 function FriendSend($sid) {
+    $yn = null;
+    $ye = null;
     global $user, $cookie, $prefix, $db, $user_prefix, $module_name;
     $sid = intval($sid);
     if(!isset($sid)) { exit(); }
     include_once(NUKE_BASE_DIR."header.php");
     $row = $db->sql_fetchrow($db->sql_query("SELECT title FROM ".$prefix."_stories WHERE sid='$sid'"));
-    $title = stripslashes(check_html($row["title"], "nohtml"));
+    $title = stripslashes((string) check_html($row["title"], "nohtml"));
     title('FRIEND');
     OpenTable();
     echo "<center><span class=\"content\"><strong>"._FRIEND."</strong></span></center><br /><br />"
@@ -54,8 +61,8 @@ function FriendSend($sid) {
         ."<input type=\"hidden\" name=\"sid\" value=\"$sid\">";
     if (is_user()) {
         $row2 = $db->sql_fetchrow($db->sql_query("SELECT name, username, user_email FROM ".$user_prefix."_users WHERE user_id = '".intval($cookie[0])."'"));
-        $yn = stripslashes($row2["name"]);
-        $ye = stripslashes($row2["user_email"]);
+        $yn = stripslashes((string) $row2["name"]);
+        $ye = stripslashes((string) $row2["user_email"]);
     }
     echo "<strong>"._FYOURNAME." </strong> <input type=\"text\" name=\"yname\" value=\"$yn\"><br /><br />\n"
         ."<strong>"._FYOUREMAIL." </strong> <input type=\"text\" name=\"ymail\" value=\"$ye\"><br /><br /><br />\n"
@@ -80,17 +87,17 @@ function SendStory($sid, $yname, $ymail, $fname, $fmail) {
         include_once(NUKE_BASE_DIR.'footer.php');
         exit;
     }
-    $fname = stripslashes(removecrlf($fname));
-    $fmail = stripslashes(removecrlf($fmail));
-    $yname = stripslashes(removecrlf($yname));
-    $ymail = stripslashes(removecrlf($ymail));
+    $fname = stripslashes((string) removecrlf($fname));
+    $fmail = stripslashes((string) removecrlf($fmail));
+    $yname = stripslashes((string) removecrlf($yname));
+    $ymail = stripslashes((string) removecrlf($ymail));
     $sid = intval($sid);
     $row = $db->sql_fetchrow($db->sql_query("SELECT title, time, topic FROM ".$prefix."_stories WHERE sid='$sid'"));
-    $title = stripslashes(check_html($row["title"], "nohtml"));
+    $title = stripslashes((string) check_html($row["title"], "nohtml"));
     $time = $row["time"];
     $topic = intval($row["topic"]);
     $row2 = $db->sql_fetchrow($db->sql_query("SELECT topictext FROM ".$prefix."_topics WHERE topicid='$topic'"));
-    $topictext = stripslashes(check_html($row2["topictext"], "nohtml"));
+    $topictext = stripslashes((string) check_html($row2["topictext"], "nohtml"));
     $subject = ""._INTERESTING." $sitename";
     $message = ""._HELLO." $fname:\n\n"._YOURFRIEND." $yname "._CONSIDERED."\n\n\n$title\n("._FDATE." $time)\n"._FTOPIC." $topictext\n\n"._URL.": $nukeurl/modules.php?name=$module_name&file=article&sid=$sid\n\n"._YOUCANREAD." $sitename\n$nukeurl";
     evo_mail($fmail, $subject, $message, "From: \"$yname\" <$ymail>\nX-Mailer: PHP/" . phpversion());
@@ -101,8 +108,8 @@ function SendStory($sid, $yname, $ymail, $fname, $fmail) {
 
 function StorySent($title, $fname) {
     include_once(NUKE_BASE_DIR."header.php");
-    $title = htmlspecialchars(urldecode(check_html($title, "nohtml")));
-    $fname = htmlspecialchars(urldecode($fname));
+    $title = htmlspecialchars(urldecode((string) check_html($title, "nohtml")));
+    $fname = htmlspecialchars(urldecode((string) $fname));
     OpenTable();
     echo "<center><span class=\"content\">"._FSTORY." <strong>$title</strong> "._HASSENT." $fname... "._THANKS."</span></center>";
     CloseTable();
